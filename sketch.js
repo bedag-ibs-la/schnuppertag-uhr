c165fa215b5c2a9e123a89ed92b9cb1c5e6ad9ff
bedag-ibs-la-user <79101505+bedag-ibs-la-user@users.noreply.github.com>
2021-05-03 15:17:45 +0200

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef, no-unused-vars */
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // TODO canvas setup code
+}
+
+function draw() {
+  // TODO drawing code
+}
+
+// This Redraws the Canvas when resized
+windowResized = function () {
+  resizeCanvas(windowWidth, windowHeight);
+};
